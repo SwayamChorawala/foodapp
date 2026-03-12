23e363570e1cdad7adfe2c4ffae14fb5983c2801
Your Name <youremail@gmail.com>
2026-03-12 20:29:38 +0530

    Refactor: implemented full project responsiveness and fixed layout bugs

--- src/components/About/Team.jsx
+++ src/components/About/Team.jsx
@@ -4,28 +4,18 @@ const Team = () => {
   return (
     <div>
       <div className='main2'>
-        <h1 id='hc'>
-        Our Team
-        </h1>
-        <img src="chef1.avif" alt="" id='chef1' />
-        <p id='pc'>    
-        Jone Lake
-        </p>
-        <p id='pc2'>
-            Chef
-        </p>
-        <div>
-            <div className='main3'>
-                <img src="chef2.avif" alt="" id='chef2' />
-            </div>
-            <p id='pc3'>
-                Rick Landry
-            </p>
-            <p id='pc4'>
-            So-Chef
-
-
-            </p>
+        <h1 id='hc'>Our Team</h1>
+        <div className="teams-grid">
+          <div className="chef-card">
+            <img src="chef1.avif" alt="Jone Lake" id='chef1' />
+            <p id='pc'>Jone Lake</p>
+            <p id='pc2'>Chef</p>
+          </div>
+          <div className="chef-card">
+            <img src="chef2.avif" alt="Rick Landry" id='chef2' />
+            <p id='pc3'>Rick Landry</p>
+            <p id='pc4'>So-Chef</p>
+          </div>
         </div>
       </div>
     </div>
