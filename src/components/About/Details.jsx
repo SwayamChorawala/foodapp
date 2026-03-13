5df5c507311bf99f4981816e3d19e5de2c1e06bf
Your Name <youremail@gmail.com>
2026-03-13 15:44:48 +0530

    done

--- src/components/About/Details.jsx
+++ src/components/About/Details.jsx
@@ -14,7 +14,7 @@ const Details = () => {
             At The chef and the table, we pride ourselves on offering a unique dining experience where every dish tells a story. Our talented chefs curate seasonal tasting menus that showcase the finest local ingredients. Step into an intimate setting where each plate is a masterpiece of precision and creativity, ensuring a dining journey that is unforgettable and delightful.
           </p>
         </div>
-        <img src="photo.jpeg" alt="" id='myfood'/>
+        <img src="photo.jpeg" alt="" id='myfood1'/>
       </div>
     </div>
   )
