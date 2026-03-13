eb17674a10b3bd9b0402371d4c94091a9fe1b3ea
Your Name <youremail@gmail.com>
2026-03-13 16:13:48 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 import { LuShoppingBag } from "react-icons/lu";
-import Card2 from '../components/Menu/Card2'
 import { useSelector } from 'react-redux';
+
 const Navbar = () => {
   const cartItems = useSelector((state) => state.app);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div>
-    <div className='navbar'>
-        <div className='nav-brand'>
-            {/* Can add logo here in the future if needed */}
+    <nav className='navbar'>
+      <div className='nav-inner'>
+        <Link to="/" className='nav-title'>The Chef & The Table</Link>
+
+        <button
+          className={`hamburger ${menuOpen ? 'active' : ''}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+          <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+          <Link to="/about" onClick={() => setMenuOpen(false)}>About</Link>
+          <Link to="/menu" onClick={() => setMenuOpen(false)}>Menu</Link>
+          <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
         </div>
-        <div className='nav-links'>
-            <Link to="/">home</Link> 
-            <Link to="/about">about</Link>
-            <Link to="/menu">Menu</Link>
-            <Link to="/contact">contact</Link>
-        </div>
+
         <div className='nav-right'>
-            <div className='Shopping-bag'>
-             <Link to="/card2"><LuShoppingBag /></Link>
-                <span className='value'>{cartItems.length}</span>
-            </div>
+          <div className='Shopping-bag'>
+            <Link to="/card2"><LuShoppingBag /></Link>
+            <span className='value'>{cartItems.length}</span>
+          </div>
         </div>
-    </div>
-    </div>
+      </div>
+    </nav>
   )
 }
 
